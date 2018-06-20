25fd7970c5fb46f70217a56581b9f0f6e196a60d
Crystal Shir Navarro <crystal.shir@gmail.com>
2018-06-19 21:11:55 -0700

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send({hi: 'herroooooooooOoOOOO'});
+    res.send({hi: 'herrOOOO hehe '});
 })
 
 app.listen(PORT, () => {
