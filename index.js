b37317a1ec112a758177e737790cb7942bd22605
Crystal Shir Navarro <crystal.shir@gmail.com>
2018-11-05 19:45:20 -0800

    Added billing to client side

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  //express will serve up production assets
+  app.use(express.static("client/build"));
+  //express will serve index.html if it doesnt recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Listening on port: ", PORT);
